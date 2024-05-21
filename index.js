4b200f6ec04e6af67bd32f884344b083a76f43e3
Anna Hankina <161770395+AnnaHanki@users.noreply.github.com>
2024-05-21 19:35:13 +0200

    add files

---
+++ index.js
@@ -0,0 +1,11 @@
+const paths = document.querySelectorAll('path');
+
+for (let i = 0; i < paths.length; i++) {
+    console.log(`Length ${i} is ${Math.ceil(paths[i].getTotalLength())}`);
+}
+
+
+// const paths = document.querySelector('path');
+// for (let i = 0; i < paths.length; i++) {
+//     console.log(`Length ${i} is ${Math.ceil(paths[i].getTotalLength())}`);
+// }
